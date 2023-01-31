ce9058cbb290997664c5b621fa754c13ca687f20
M <chotchat.c@codium.co>
2023-01-31 18:20:07 +0700

    🔧 FE: Updated environments baseImg & placeholder Image

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
   baseUrl: 'https://api.artic.edu',
+  baseImgUrl: '/full/843,/0/default.jpg',
+  placeholderImgUrl:
+    'https://upload.wikimedia.org/wikipedia/commons/3/32/Art_Institute_of_Chicago_logo.svg',
 };
