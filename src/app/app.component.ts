adf0b241e19049a7e14516572cff9f313123e4c4
M <chotchat.c@codium.co>
2023-01-31 17:30:45 +0700

    🔨 FE: Updated script cycle component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+}
 
 /** Copyright Allianz 2023 */
