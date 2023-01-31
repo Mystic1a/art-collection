37411e54c27e65d6b16aa982d27599007f556da8
M <chotchat.c@codium.co>
2023-01-31 18:43:40 +0700

    ✨ FE: Added Business Logic Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,85 @@
+import { environment } from 'src/environments/environments';
+import { ArtCollectionService } from './service/art-collection.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Subject,
+  Subscription,
+  switchMap,
+} from 'rxjs';
+import {
+  ArtCollectionConfig,
+  ArtCollectionData,
+  ArtCollectionModel,
+  ArtCollectionParam,
+  Pagination,
+} from './model/art-collection.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor() {}
+  private subscription: Subscription = new Subscription();
+
+  artCollectionList: ArtCollectionData[] = [];
+  artCollectionList$ = new Subject<ArtCollectionParam>();
+  artCollectionConfig!: ArtCollectionConfig;
+  artCollectionPagination!: Pagination;
+
+  onSetBaseImgUrl = environment.baseImgUrl;
+  onSetPlaceholderImg = environment.placeholderImgUrl;
+
+  styleTitlesDropdown: string[] = [];
+  selectedStylesTitle: string[] = [];
+
+  pageStart = 1;
+  perPage = 10;
+  isLoading = true;
+
+  constructor(private artCollectionService: ArtCollectionService) {}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.subscribeArtWorkCollection();
+    this.takeArtWorkCollection();
   }
+
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+    this.subscription.unsubscribe();
+  }
+
+  subscribeArtWorkCollection(): void {
+    this.subscription.add(
+      this.artCollectionList$
+        .pipe(
+          distinctUntilChanged(),
+          debounceTime(300),
+          switchMap((params) =>
+            this.artCollectionService.onGetArtWorkList(params)
+          )
+        )
+        .subscribe({
+          next: (response: ArtCollectionModel) => {
+            this.artCollectionList = response.data;
+            this.artCollectionConfig = response.config;
+            this.artCollectionPagination = response.pagination;
+            this.isLoading = false;
+          },
+          error: (err) => {
+            console.error(err);
+            this.isLoading = false;
+          },
+        })
+    );
+  }
+
+  takeArtWorkCollection(): void {
+    const params: ArtCollectionParam = {
+      page: this.pageStart,
+    };
+    this.isLoading = true;
+    this.artCollectionList$.next(params);
   }
 }
 
